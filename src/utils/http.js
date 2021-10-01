064efa9faf2843ce36a800616d5c2a64d3402c21
tangxue <11183504@qq.com>
2021-10-01 16:13:59 +0800

    feat: test

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,23 +1,24 @@
-import qs from "qs";
-import axios from "axios";
-import { cleanObject } from "./index";
+import qs from 'qs'
+import axios from 'axios'
+import { cleanObject } from './index'
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = process.env.REACT_APP_API_URL
 
-axios.defaults.baseURL = apiUrl;
-export const http = (u, m = "get", p) => {
-  m = m.toUpperCase();
-  const data = cleanObject(p);
-  let url = "";
-  if (m === "GET") {
-    console.log(222);
+axios.defaults.baseURL = apiUrl
+export const http = (u, m = 'get', p) => {
+  m = m.toUpperCase()
+  const data = cleanObject(p)
+  let url = ''
+
+  if (m === 'GET') {
+    console.log(222)
     url =
-      u.indexOf("?") === -1
+      u.indexOf('?') === -1
         ? `${u}?${qs.stringify(data)}`
-        : `${u}&${qs.stringify(data)}`;
+        : `${u}&${qs.stringify(data)}`
   }
   return axios({
     url,
     method: m,
-  });
-};
+  })
+}
