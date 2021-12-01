a3f2742adb38a39e38bcfa5e8585b26030f82f06
tangxve <11183504@qq.com>
2021-12-01 21:45:07 +0800

    feat: js 切换到 ts

---
+++ src/screens/projet-list/index.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import { SearchPanel } from './search-panel'
+import { List } from './list'
+import { http } from '../../utils/http'
+import { useMount, useDebounce } from '../../utils'
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({ name: '', personId: '' })
+  const [users, setUsers] = useState([])
+  const [list, setList] = useState([])
+
+  const debounceParam = useDebounce(param, 500)
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await http('/projects', 'get', { ...debounceParam })
+      const { data, status } = res
+
+      if (status === 200) {
+        setList(data)
+      }
+    }
+
+    fetchData()
+  }, [debounceParam])
+
+  useMount(() => {
+    async function fetchData() {
+      const res = await http('/users', 'get')
+
+      const { data, status } = res
+
+      if (status === 200) {
+        setUsers(data)
+      }
+    }
+
+    fetchData()
+  })
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List users={users} list={list} />
+    </div>
+  )
+}
---
+++ src/screens/projet-list/search-panel.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+export interface User {
+  id: string
+  name: string
+  email: string
+  title: string
+  organization: string
+}
+
+interface SearchPanelProps {
+  users: User[]
+  param: {
+    name: string
+    personId: string
+  }
+  setParam: (param: SearchPanelProps['param']) => void
+}
+
+export const SearchPanel: React.FC<SearchPanelProps> = ({
+  param,
+  setParam,
+  users,
+}) => {
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(event) =>
+            setParam({
+              ...param,
+              name: event.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(event) =>
+            setParam({
+              ...param,
+              personId: event.target.value,
+            })
+          }
+        >
+          <option value={''}>负责人</option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  )
+}
